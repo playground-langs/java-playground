55f08e9120d208a6485796f254a39f20bb3a265c
techzealot <techzealot@foxmail.com>
2022-04-18 17:03:28 +0800

    新增用例 StackWalker api

--- src/test/java/com/techzealot/examples/features/StackWalkerTest.java
+++ src/test/java/com/techzealot/examples/features/StackWalkerTest.java
@@ -1,6 +1,26 @@
 package com.techzealot.examples.features;
 
+import org.junit.jupiter.api.Test;
+
 public class StackWalkerTest {
-    public static void main(String[] args) {
+
+
+    public String getCurrentCallerMethodName() {
+        StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+        StackWalker.StackFrame frame = walker.walk((stream) -> stream.skip(1).findFirst().orElse(null));
+        return frame == null ? "null" : frame.getMethodName();
+    }
+
+    public void printCallStack(){
+        StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+        walker.forEach((frame)->{
+            System.out.println(frame.getClassName()+"#"+frame.getMethodName());
+        });
+    }
+
+    @Test
+    public void testStackWalker(){
+        System.out.println(getCurrentCallerMethodName());
+        printCallStack();
     }
 }
